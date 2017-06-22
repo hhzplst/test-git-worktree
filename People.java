546f13ec9e61654ff94a4bda6dc88e9f12ac3bd7
Ivy Chang <ivycwq@icloud.com>
2017-06-22 11:41:39 -0700

    adding people class

---
+++ People.java
@@ -0,0 +1,13 @@
+public class People {
+	String firstName;
+	String lastName;
+	int age;
+
+	public setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public getFirstname() {
+		return firstName;
+	}
+}
